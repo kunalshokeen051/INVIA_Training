136cf2bd6e4f351151dcb683fab85ae061886096
kunalshokeen <kunalshokeen051@gmail.com>
2023-08-29 08:54:45 +0530

    Day14-code-updated

---
+++ Class_Code/Day14/P2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+public class Browse
+{
+   public void Details()
+    {
+        Assembly ptr = Assembly.LoadFrom(@"\Desktop\MyApp.dll");
+      Type[] t =  ptr.GetTypes();
+        foreach (var i in t)
+        {
+            Console.WriteLine(i);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Browse obj = new Browse();
+        obj.Details();
+    }
+}
+
